c727a7a6f9b4390d5ccc7af5aac37c1c6775aea4
SaukiFutaki <utaki.sauki19@gmail.com>
2024-07-28 19:05:29 +0700

    feat : flatlist

--- components/FoodItemList.tsx
+++ components/FoodItemList.tsx
@@ -1,33 +1,34 @@
-import { View,Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 interface FoodListItemProps {
-    item: {
-      label: string;
-      cal: number;
-      brand: string;
-    };
-  }
-  
-export  default function FoodListItem ({ item }: FoodListItemProps){
-    return (
-      <View
-        style={{
-          backgroundColor: "gainsboro",
-          padding: 10,
-          borderRadius: 5,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <View style={{ flex: 1, gap: 5 }}>
-          <Text style={{ fontWeight: "bold", fontSize: 16 }}>{item.label}</Text>
-          <Text style={{ fontSize: 14, color: "dimgray" }}>
-            {item.cal} cal, {item.brand}
-          </Text>
-        </View>
-        <AntDesign name="pluscircleo" size={24} color="royalblue" />
-      </View>
-    );
+  item: {
+    label: string;
+    cal: number;
+    brand: string;
   };
-  
+}
+
+export default function FoodListItem({ item }: FoodListItemProps) {
+  return (
+    <View style={style.container}>
+      <View style={{ flex: 1, gap: 5 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 16 }}>{item.label}</Text>
+        <Text style={{ fontSize: 14, color: "dimgray" }}>
+          {item.cal} cal, {item.brand}
+        </Text>
+      </View>
+      <AntDesign name="pluscircleo" size={24} color="royalblue" />
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: "gainsboro",
+    padding: 10,
+    borderRadius: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,57 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
 import { AntDesign } from "@expo/vector-icons";
 import FoodListItem from "@/components/FoodItemList";
 
+const foodList = [
+  {
+    label: "Apple",
+    cal: 95,
+    brand: "Dole",
+  },
+  {
+    label: "Banana",
+    cal: 105,
+    brand: "Dole",
+  },
+  {
+    label: "Orange",
+    cal: 62,
+    brand: "Dole",
+  },
+  {
+    label: "Peach",
+    cal: 59,
+    brand: "Dole",
+  },
+  {
+    label: "Pineapple",
+    cal: 50,
+    brand: "Dole",
+  },
+  {
+    label: "Strawberry",
+    cal: 33,
+    brand: "Dole",
+  },
+  {
+    label: "Watermelon",
+    cal: 30,
+    brand: "Dole",
+  },
+];
+
 export default function Home() {
   return (
     <View style={styles.container}>
       {/* FOOD item view*/}
-      <FoodListItem
-        item={{
-          label: "Apple",
-          cal: 95,
-          brand: "Dole",
-        }}
-      />
-      <FoodListItem
-        item={{
-          label: "Banana",
-          cal: 105,
-          brand: "Dole",
-        }}
+
+      <FlatList
+        data={foodList}
+        renderItem={({ item }) => <FoodListItem item={item} />}
+        contentContainerStyle={{ gap: 5 }}
       />
     </View>
   );
@@ -36,4 +66,3 @@ const styles = StyleSheet.create({
     gap: 7,
   },
 });
-
